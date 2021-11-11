d59f0c864c9fb149ca892880a26821d2d5545484
LeDixiemeHomme <benoit.valleea@hotmail.fr>
2021-11-11 17:00:37 +0100

    feat(display): create Viewer class for screen and display related methods

---
+++ display/Viewer.py
@@ -0,0 +1,35 @@
+import pygame
+
+from project.MetaSingleton.MetaSingleton import MetaSingleton
+
+
+class Viewer(metaclass=MetaSingleton):
+
+    def __init__(self):
+        pygame.init()
+        self.__screen = self.__init_screen()
+        self.__display = self.__init_display()
+
+    @staticmethod
+    def __init_screen():
+        screen = pygame.display
+        screen.set_caption("Hardest game IA")
+        screen.set_icon(pygame.image.load('./static/artificial-intelligence.png'))
+        return screen
+
+    def __init_display(self):
+        my_display = self.__screen.set_mode((800, 800))
+        return my_display
+
+    def draw(self, color: (int, int, int), rect: pygame.Rect):
+        pygame.draw.rect(self.__display, color, rect)
+
+    @property
+    def display(self):
+        return self.__display
+
+    @property
+    def screen(self):
+        return self.__screen
+
+
