303abb73e6df145e2ccf10465e130236c76b7650
LeDixiemeHomme <benoit.valleea@hotmail.fr>
2021-11-11 17:09:23 +0100

    feat(obstacle): create class related to obstacle creation

---
+++ project/model/Pattern.py
@@ -0,0 +1,13 @@
+from typing import List
+
+from project.model.Movement import Movement
+
+
+class Pattern:
+
+    def __init__(self, list_of_movements: List[Movement]):
+        self.__list_of_movements = list_of_movements
+
+    @property
+    def list_of_movements(self):
+        return self.__list_of_movements
