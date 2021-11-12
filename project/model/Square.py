4d9a754f067cfd9fac9fd8c21449e90c85d1dca8
LeDixiemeHomme <benoit.valleea@hotmail.fr>
2021-11-12 10:50:15 +0100

    fix(square): update is_position_inside method, now working

--- project/model/Square.py
+++ project/model/Square.py
@@ -9,13 +9,8 @@ class Square:
         self.__square_type = square_type
 
     def is_position_inside(self, position_to_test: Position) -> bool:
-        test1 = self.__position.co_x <= position_to_test.co_x
-        test2 = position_to_test.co_x <= self.__position.co_x + constant.SQUARE_SIZE * 0.01
-        test3 = self.__position.co_y <= position_to_test.co_y
-        test4 = position_to_test.co_y <= self.__position.co_y + constant.SQUARE_SIZE * 0.01
-        print(test1, test2, test3, test4)
-        return self.__position.co_x < position_to_test.co_x < self.__position.co_x + constant.SQUARE_SIZE * 0.01 \
-               and self.__position.co_y < position_to_test.co_y < self.__position.co_y + constant.SQUARE_SIZE * 0.01
+        return self.__position.co_x <= position_to_test.co_x <= self.__position.co_x + constant.SQUARE_SIZE \
+               and self.__position.co_y <= position_to_test.co_y <= self.__position.co_y + constant.SQUARE_SIZE
 
     @property
     def position(self):
@@ -25,5 +20,6 @@ class Square:
     def square_type(self):
         return self.__square_type
 
-    def __repr__(self):
-        return "Square : square_type = " + str(self.__square_type)
+    def __str__(self) -> str:
+        string: str = "Square :" + " position = " + str(self.__position) + " square_type = " + str(self.__square_type)
+        return string
