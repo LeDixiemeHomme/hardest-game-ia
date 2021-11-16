084c60e8bd6419788d9edfadd0004064f79e53d7
LeDixiemeHomme <benoit.valleea@hotmail.fr>
2021-11-16 15:00:40 +0100

    fix(Viewer): remove draw_board method

--- project/display/Viewer.py
+++ project/display/Viewer.py
@@ -1,9 +1,6 @@
 import pygame
 
 from project.metaSingleton.MetaSingleton import MetaSingleton
-from project.model import constant
-from project.model.Board import Board
-from project.model.SquareType import SquareType
 
 
 class Viewer(metaclass=MetaSingleton):
@@ -31,22 +28,6 @@ class Viewer(metaclass=MetaSingleton):
     def draw(self, color: (int, int, int), rect: pygame.Rect):
         pygame.draw.rect(self.__display, color, rect)
 
-    @staticmethod
-    def draw_board(board: Board):
-        size = constant.SQUARE_SIZE * constant.DRAW_SCALE
-        color = constant.COLOR
-        for square in board.list_of_square:
-            rect = pygame.Rect(square.position.co_x * constant.DRAW_SCALE, square.position.co_y * constant.DRAW_SCALE,
-                               size, size)
-            if square.square_type == SquareType.GOAL:
-                Viewer().draw(color=color.get("GREEN"), rect=rect)
-            elif square.square_type == SquareType.START:
-                Viewer().draw(color=color.get("RED"), rect=rect)
-            elif square.square_type == SquareType.EMPTY:
-                Viewer().draw(color=color.get("WHITE"), rect=rect)
-            else:
-                Viewer().draw(color=color.get("BLACK"), rect=rect)
-
     @property
     def display(self):
         return self.__display
@@ -54,5 +35,3 @@ class Viewer(metaclass=MetaSingleton):
     @property
     def screen(self):
         return self.__screen
-
-
