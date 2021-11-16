9e60e2d3d04b470dfd17f5e7d6483dba3bae11c4
LeDixiemeHomme <benoit.valleea@hotmail.fr>
2021-11-16 15:02:28 +0100

    feat(SquareTests): implement tests for is_position_inside method of the Square class

---
+++ tests/SquareTests.py
@@ -0,0 +1,37 @@
+import unittest
+
+from project.model.Position import Position
+from project.model.Square import Square
+from project.model.SquareType import SquareType
+
+
+class SquareTests(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.square: Square = Square(Position(5.0, 5.0), SquareType.EMPTY)
+        coordinates: [[float, float]] = [[5.01, 5.01], [0.0, 0.0], [10.0, 10.0], [0.0, 10.0], [10.0, 0.0], [4.9, 4.7]]
+        self.positions: [Position] = []
+        for position in coordinates:
+            self.positions.append(Position(co_x=position[0], co_y=position[1]))
+
+    def test_should_is_position_inside_with_position_0_return_true(self):
+        self.assertTrue(self.square.is_position_inside(self.positions[0]))
+
+    def test_should_is_position_inside_with_position_1_return_false(self):
+        self.assertFalse(self.square.is_position_inside(self.positions[1]))
+
+    def test_should_is_position_inside_with_position_2_return_false(self):
+        self.assertFalse(self.square.is_position_inside(self.positions[2]))
+
+    def test_should_is_position_inside_with_position_3_return_false(self):
+        self.assertFalse(self.square.is_position_inside(self.positions[3]))
+
+    def test_should_is_position_inside_with_position_4_return_false(self):
+        self.assertFalse(self.square.is_position_inside(self.positions[4]))
+
+    def test_should_is_position_inside_with_position_5_return_false(self):
+        self.assertFalse(self.square.is_position_inside(self.positions[5]))
+
+
+if __name__ == '__main__':
+    unittest.main()
