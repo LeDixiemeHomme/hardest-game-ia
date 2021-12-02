1cc5058eaed13dd4f6d7b110a511b0e656993b83
LeDixiemeHomme <benoit.valleea@hotmail.fr>
2021-12-02 07:50:28 +0100

    feat(refacto): add Position class, SquareType class and OutOfBoundBlockPosition exception to the square.py file

--- project/exception/out_of_bond_block_position.py
+++
@@ -1,8 +0,0 @@
-from project.model.position import Position
-from project.model.square_type import SquareType
-
-
-class OutOfBoundBlockPosition(Exception):
-    def __init__(self, position: Position, square_type: SquareType, width: int, height: int):
-        super().__init__(
-            f'Position {position} of the block {square_type} is out of bound. Width = {width},Height = {height}')
--- project/model/square_type.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class SquareType(Enum):
-    START = 1
-    GOAL = 2
-    EMPTY = 3
-    WALL = 4
-    OBSTACLE = 5
