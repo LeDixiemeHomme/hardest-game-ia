6403beea776cdbfaf7ea4fc2a9a9f1fc6747c774
LeDixiemeHomme <benoit.valleea@hotmail.fr>
2021-12-15 19:35:32 +0100

    feat(refacto_board): change some methods' accessibility level and refacto the init of board class

--- project/model/board.py
+++ project/model/board.py
@@ -4,6 +4,7 @@ from project.custom_exception.none_instantiate_singleton_viewer_exception import
 from project.custom_exception.out_of_bound_block_position_exception import OutOfBoundBlockPositionException
 
 from project.constants import constants
+from project.custom_exception.wrong_display_size_exception import WrongDisplaySizeException
 from project.logger.logger import Logger
 from project.display.viewer import Viewer
 from project.model.agent import Agent
@@ -23,32 +24,33 @@ class Board:
     def __init__(self, width: int, height: int, position_start: Position, position_goal: Position,
                  list_of_obstacle: List[Obstacle], agent: Agent):
         self._viewer = None
-        stdout_logger.debug("init board")
-        self._position_start = position_start
-        self._position_goal = position_goal
-        self._list_of_obstacle: List[Obstacle] = list_of_obstacle
-        self._width = width
-        self._height = height
         self._agent = agent
-        self._init_start()
-        self._init_goal()
-        self._init_list_of_obstacle()
+        self._check_board_size(width=width, height=height)
+        self._check_init_position(position_start=position_start, position_goal=position_goal,
+                                  list_of_obstacle=list_of_obstacle)
         self._list_of_square: List[Square] = self._init_list_of_square()
 
-    def instantiate_singleton_viewer(self):
-        self._viewer: Viewer = Viewer(width=self._width, height=self._height)
-
-    def _init_start(self) -> SquareType:
-        if not self.is_position_inside_board_boundaries(self._position_start):
-            raise OutOfBoundBlockPositionException(position=self._position_start, square_type=SquareType.START,
+    def _check_boundaries(self, tested_position: Position, square_type_tested: SquareType):
+        if not self._is_position_inside_board_boundaries(position_to_test=tested_position):
+            raise OutOfBoundBlockPositionException(position=tested_position, square_type=square_type_tested,
                                                    width=self._width, height=self._height)
-        return SquareType.START
 
-    def _init_goal(self) -> SquareType:
-        if not self.is_position_inside_board_boundaries(self._position_goal):
-            raise OutOfBoundBlockPositionException(position=self._position_goal, square_type=SquareType.GOAL,
-                                                   width=self._width, height=self._height)
-        return SquareType.GOAL
+    def _check_init_position(self, position_start: Position, position_goal: Position, list_of_obstacle: List[Obstacle]):
+        self._check_boundaries(tested_position=position_start, square_type_tested=SquareType.START)
+        self._position_start = position_start
+
+        self._check_boundaries(tested_position=position_goal, square_type_tested=SquareType.GOAL)
+        self._position_goal = position_goal
+
+        for obstacle in list_of_obstacle:
+            self._check_boundaries(tested_position=obstacle.position, square_type_tested=SquareType.OBSTACLE)
+        self._list_of_obstacle: List[Obstacle] = list_of_obstacle
+
+    def _check_board_size(self, width: int, height: int):
+        if width > 20 or height > 20:
+            raise WrongDisplaySizeException(width, height)
+        self._width = width
+        self._height = height
 
     def _init_list_of_square(self) -> List[Square]:
         list_of_square = []
@@ -57,34 +59,61 @@ class Board:
             for y in range(1, self._height + 1):
                 list_of_square.append(Square(position=Position(x, y), square_type=SquareType.EMPTY))
 
-        list_of_square[self.get_index_of_list_of_square_by_position(self._position_start)] = \
+        list_of_square[self._get_index_of_list_of_square_by_position(self._position_start)] = \
             Square(position=self._position_start, square_type=SquareType.START)
 
-        list_of_square[self.get_index_of_list_of_square_by_position(self._position_goal)] = \
+        list_of_square[self._get_index_of_list_of_square_by_position(self._position_goal)] = \
             Square(position=self._position_goal, square_type=SquareType.GOAL)
 
         for obstacle in self._list_of_obstacle:
             obstacle_position = obstacle.position
-            list_of_square[self.get_index_of_list_of_square_by_position(position=obstacle_position)] = \
+            list_of_square[self._get_index_of_list_of_square_by_position(position=obstacle_position)] = \
                 Square(position=obstacle_position, square_type=SquareType.OBSTACLE)
 
         return list_of_square
 
-    def _init_list_of_obstacle(self) -> List[Obstacle]:
-        for obstacle in self._list_of_obstacle:
-            if not self.is_position_inside_board_boundaries(obstacle.position):
-                raise OutOfBoundBlockPositionException(position=obstacle.position, square_type=SquareType.OBSTACLE,
-                                                       width=self._width, height=self._height)
-        return self._list_of_obstacle
+    def _get_square_type_from_board_by_position(self, position: Position) -> SquareType:
+        self._check_boundaries(tested_position=position, square_type_tested=SquareType.OBSTACLE)
+        return self._list_of_square[
+            self._get_index_of_list_of_square_by_position(position)].square_type
+
+    def _get_index_of_list_of_square_by_position(self, position: Position) -> int:
+        self._check_boundaries(tested_position=position, square_type_tested=SquareType.OBSTACLE)
+        return self._height * position.co_x - (self._height - position.co_y) - 1
+
+    def _get_position_after_movement(self, current_position: Position, current_movement: Movement) -> Position:
+        self._check_boundaries(tested_position=current_position, square_type_tested=SquareType.OBSTACLE)
+        position_after_movement: Position = Position(co_x=current_position.co_x, co_y=current_position.co_y)
+
+        direction: Direction = current_movement.direction
+        length: int = current_movement.length
+
+        if direction == Direction.UP:
+            position_after_movement.co_y -= length
+        elif direction == Direction.DOWN:
+            position_after_movement.co_y += length
+        elif direction == Direction.LEFT:
+            position_after_movement.co_x -= length
+        elif direction == Direction.RIGHT:
+            position_after_movement.co_x += length
+
+        return position_after_movement
+
+    def _is_position_inside_board_boundaries(self, position_to_test: Position) -> bool:
+        return 0 < position_to_test.co_x <= self._width and 0 < position_to_test.co_y <= self._height
 
     def is_agent_position_on_goal_square(self):
         return self._agent.position == self._position_goal
 
     def is_agent_position_on_obstacle_square(self):
         for obstacle in self._list_of_obstacle:
-            if self._agent.position == obstacle.position:
+            if obstacle.is_position_inside(position_to_test=self._agent.position):
                 return True
         return False
+
+    def instantiate_singleton_viewer(self):
+        stdout_logger.debug("Instantiate board singleton viewer ...")
+        self._viewer: Viewer = Viewer(width=self._width, height=self._height)
 
     def draw_agent(self):
         self._agent.draw(viewer=self.viewer)
@@ -94,9 +123,9 @@ class Board:
         current_position_type: SquareType = self._agent.temp_type
         current_movement: Movement = Movement(direction=direction)
         try:
-            next_position: Position = self.get_position_after_movement(current_position=current_position,
-                                                                       current_movement=current_movement)
-            next_square_type: SquareType = self.get_square_type_from_board_by_position(position=next_position)
+            next_position: Position = self._get_position_after_movement(current_position=current_position,
+                                                                        current_movement=current_movement)
+            next_square_type: SquareType = self._get_square_type_from_board_by_position(position=next_position)
         except OutOfBoundBlockPositionException:
             next_position: Position = current_position
             next_square_type = current_position_type
@@ -113,9 +142,9 @@ class Board:
             current_movement: Movement = obstacle.pattern.list_of_movements[
                 obstacle.pattern_state % len(obstacle.pattern.list_of_movements)]
             try:
-                next_position: Position = self.get_position_after_movement(current_position=current_position,
-                                                                           current_movement=current_movement)
-                next_square_type: SquareType = self.get_square_type_from_board_by_position(position=next_position)
+                next_position: Position = self._get_position_after_movement(current_position=current_position,
+                                                                            current_movement=current_movement)
+                next_square_type: SquareType = self._get_square_type_from_board_by_position(position=next_position)
             except OutOfBoundBlockPositionException:
                 next_position: Position = current_position
                 next_square_type = current_position_type
@@ -134,49 +163,12 @@ class Board:
                 left_arg=square.position.co_x, top_arg=square.position.co_y)
             self.viewer.draw(color=constants.COLOR_WITH_TYPE.get(square.square_type), rect=rect)
 
-    def get_square_type_from_board_by_position(self, position: Position) -> SquareType:
-        if not self.is_position_inside_board_boundaries(position_to_test=position):
-            raise OutOfBoundBlockPositionException(position=position, square_type=SquareType.OBSTACLE,
-                                                   width=self._width, height=self._height)
-        return self._list_of_square[
-            self.get_index_of_list_of_square_by_position(position)].square_type
-
-    def get_index_of_list_of_square_by_position(self, position: Position) -> int:
-        if not self.is_position_inside_board_boundaries(position_to_test=position):
-            raise OutOfBoundBlockPositionException(position=position, square_type=SquareType.OBSTACLE,
-                                                   width=self._width, height=self._height)
-        return self._height * position.co_x - (self._height - position.co_y) - 1
-
-    def get_position_after_movement(self, current_position: Position, current_movement: Movement) -> Position:
-        if not self.is_position_inside_board_boundaries(position_to_test=current_position):
-            raise OutOfBoundBlockPositionException(position=current_position, square_type=SquareType.OBSTACLE,
-                                                   width=self._width, height=self._height)
-
-        position_after_movement: Position = Position(co_x=current_position.co_x, co_y=current_position.co_y)
-
-        direction = current_movement.direction
-        length = current_movement.length
-
-        if direction == Direction.UP:
-            position_after_movement.co_y -= length
-        elif direction == Direction.DOWN:
-            position_after_movement.co_y += length
-        elif direction == Direction.LEFT:
-            position_after_movement.co_x -= length
-        elif direction == Direction.RIGHT:
-            position_after_movement.co_x += length
-
-        return position_after_movement
-
-    def is_position_inside_board_boundaries(self, position_to_test: Position) -> bool:
-        return 0 < position_to_test.co_x <= self._width and 0 < position_to_test.co_y <= self._height
-
     @property
     def list_of_square(self):
         return self._list_of_square
 
     def update_list_of_square(self, position: Position, square_type: SquareType):
-        self._list_of_square[self.get_index_of_list_of_square_by_position(position=position)] = \
+        self._list_of_square[self._get_index_of_list_of_square_by_position(position=position)] = \
             Square(position=position, square_type=square_type)
 
     @property
@@ -198,6 +190,10 @@ class Board:
     @property
     def position_goal(self):
         return self._position_goal
+
+    @property
+    def agent(self):
+        return self._agent
 
     @property
     def viewer(self):
--- tests/test_board.py
+++ tests/test_board.py
@@ -4,6 +4,7 @@ from typing import List
 import pytest
 
 from project.custom_exception.out_of_bound_block_position_exception import OutOfBoundBlockPositionException
+from project.custom_exception.wrong_display_size_exception import WrongDisplaySizeException
 from project.model.agent import Agent
 from project.model.board import Board, Position, SquareType, Obstacle
 from project.model.direction import Direction
@@ -50,36 +51,36 @@ class TestBoard:
                                                                        (SquareType.EMPTY, position_empty)])
     def test_should_get_square_type_with_position_corresponding_square_type(self, excepted_square_type,
                                                                             tested_position):
-        assert excepted_square_type.value == self.board.get_square_type_from_board_by_position(tested_position).value
+        assert excepted_square_type.value == self.board._get_square_type_from_board_by_position(tested_position).value
 
     @pytest.mark.parametrize("tested_position", [position_out_under_under,
                                                  position_out_over_over,
                                                  position_out_under_in])
     def test_should_get_square_type_with_position_raise_exception(self, tested_position):
         with pytest.raises(OutOfBoundBlockPositionException):
-            self.board.get_square_type_from_board_by_position(tested_position)
+            self.board._get_square_type_from_board_by_position(tested_position)
 
     @pytest.mark.parametrize("tested_position", [position_out_under_under, position_out_under_in,
                                                  position_out_under_over, position_out_in_under,
                                                  position_out_over_under, position_out_over_over])
     def test_should_get_square_type_with_position_corresponding_square_type(self, tested_position):
-        assert not self.board.is_position_inside_board_boundaries(position_to_test=tested_position)
+        assert not self.board._is_position_inside_board_boundaries(position_to_test=tested_position)
 
     def test_should_is_position_inside_board_boundaries_with_position_start_return_true(self):
-        assert self.board.is_position_inside_board_boundaries(position_to_test=self.position_start)
+        assert self.board._is_position_inside_board_boundaries(position_to_test=self.position_start)
 
     def test_should_get_index_of_list_of_square_by_position_with_position_start_return_0(self):
         expected_index: int = 0
-        assert expected_index == self.board.get_index_of_list_of_square_by_position(position=self.position_start)
+        assert expected_index == self.board._get_index_of_list_of_square_by_position(position=self.position_start)
 
     def test_should_get_index_of_list_of_square_by_position_with_position_start_raise_exception(self):
         with pytest.raises(OutOfBoundBlockPositionException):
-            self.board.get_index_of_list_of_square_by_position(position=self.position_out_over_over)
+            self.board._get_index_of_list_of_square_by_position(position=self.position_out_over_over)
 
     def test_should_get_position_after_movement_with_position_out_raise_exception(self):
         with pytest.raises(OutOfBoundBlockPositionException):
-            self.board.get_position_after_movement(current_position=self.position_out_over_over,
-                                                   current_movement=self.right_movement)
+            self.board._get_position_after_movement(current_position=self.position_out_over_over,
+                                                    current_movement=self.right_movement)
 
     @pytest.mark.parametrize("movement, position_expected",
                              [(up_movement, Position(co_x=position_empty.co_x, co_y=position_empty.co_y - 1)),
@@ -87,15 +88,15 @@ class TestBoard:
                               (left_movement, Position(co_x=position_empty.co_x - 1, co_y=position_empty.co_y)),
                               (right_movement, Position(co_x=position_empty.co_x + 1, co_y=position_empty.co_y))])
     def test_should_get_position_after_movement(self, movement, position_expected):
-        result_position: Position = self.board.get_position_after_movement(current_position=self.position_empty,
-                                                                           current_movement=movement)
+        result_position: Position = self.board._get_position_after_movement(current_position=self.position_empty,
+                                                                            current_movement=movement)
         assert position_expected == result_position
 
     def test_should_move_obstacles_not_erase_start_position(self):
         expected_log: List[SquareType] = [SquareType.START, SquareType.OBSTACLE, SquareType.START]
         square_type_log: List[SquareType] = []
         for i in range(3):
-            square_type_log.append(self.board.get_square_type_from_board_by_position(self.position_start))
+            square_type_log.append(self.board._get_square_type_from_board_by_position(self.position_start))
             self.board.move_obstacles()
         assert expected_log == square_type_log
 
@@ -107,6 +108,16 @@ class TestBoard:
             position_log.append(self.board.list_of_obstacle[1].position)
             self.board.move_obstacles()
         assert expected_log == position_log
+
+    def test_board_with_big_width_raise_wrong_display_size_exception(self):
+        with pytest.raises(WrongDisplaySizeException):
+            Board(width=21, height=10, position_start=Position(1, 1), position_goal=Position(2, 2),
+                  list_of_obstacle=[], agent=Agent(position=Position(1, 2)))
+
+    def test_board_with_big_height_raise_wrong_display_size_exception(self):
+        with pytest.raises(WrongDisplaySizeException):
+            Board(width=10, height=21, position_start=Position(1, 1), position_goal=Position(2, 2),
+                  list_of_obstacle=[], agent=Agent(position=Position(1, 2)))
 
 
 if __name__ == '__main__':
