cdda347e6cefafa7c70c7cbce1f2ecd0911f19c0
LeDixiemeHomme <benoit.valleea@hotmail.fr>
2021-12-23 15:19:56 +0100

    fix(obstacle-move): refacto list_of_obstacle attribute of board in a specific class

--- tests/test_agent.py
+++ tests/test_agent.py
@@ -1,15 +1,35 @@
+from typing import List
+from collections import namedtuple
+
+import pytest
+
 from project.model.agent import Agent
 from project.model.board import Board, Position
+from project.model.direction import Direction
+from project.model.movement import Movement
 from project.model.obstacle import Obstacle
 
 
 class TestAgent:
     board_height: int = 5
     board_width: int = 5
+    up_movement: Movement = Movement(direction=Direction.UP)
+    down_movement: Movement = Movement(direction=Direction.DOWN)
+    left_movement: Movement = Movement(direction=Direction.LEFT)
+    right_movement: Movement = Movement(direction=Direction.RIGHT)
+    movements: List[Movement] = [up_movement, down_movement, right_movement, left_movement]
+
+    movements_to_times_five_wall_positions: (Movement, Position) = [
+        (up_movement, Position(co_x=3, co_y=1)),
+        (down_movement, Position(co_x=3, co_y=5)),
+        (left_movement, Position(co_x=1, co_y=3)),
+        (right_movement, Position(co_x=5, co_y=3))
+    ]
 
     position_start: Position = Position(1, 1)
     position_goal: Position = Position(5, 5)
-    position_obstacle: Position = Position(2, 2)
+    centered_position: Position = Position(co_x=3, co_y=3)
+    position_obstacle: Position = Position(1, 5)
 
     obstacle: Obstacle = Obstacle(position=position_obstacle)
 
@@ -23,6 +43,28 @@ class TestAgent:
         agent: Agent = Agent(board=self.board, position=self.position_goal)
         assert agent.is_position_on_goal_square()
 
-    def test_should_is_position_on_obstacle_square_with_agent_on_position_obstacle(self):
+    def test_should_is_position_on_goal_square_with_agent_on_position_goal_return_false(self):
+        agent: Agent = Agent(board=self.board, position=self.position_start)
+        assert not agent.is_position_on_goal_square()
+
+    def test_should_is_position_on_obstacle_square_with_agent_on_position_obstacle_return_true(self):
         agent: Agent = Agent(board=self.board, position=self.position_obstacle)
         assert agent.is_position_on_obstacle_square()
+
+    def test_should_is_position_on_obstacle_square_with_agent_on_position_obstacle_return_false(self):
+        agent: Agent = Agent(board=self.board, position=self.position_start)
+        assert not agent.is_position_on_obstacle_square()
+
+    @pytest.mark.parametrize("movements", movements)
+    def test_should_move_agent_if_possible_updates_agent_position(self, movements):
+        agent: Agent = Agent(board=self.board, position=self.centered_position)
+        agent.move_agent_if_possible(requested_movement=movements)
+        assert agent.position == self.centered_position.apply_movement(movement=movements)
+
+    @pytest.mark.parametrize("movements_to_times_five, wall_positions", movements_to_times_five_wall_positions)
+    def test_should_move_agent_if_possible_stop_on_wall(self, movements_to_times_five, wall_positions):
+        agent: Agent = Agent(board=self.board, position=self.centered_position)
+        for _ in range(5):
+            agent.move_agent_if_possible(requested_movement=movements_to_times_five)
+        print(agent.position, wall_positions)
+        assert agent.position == wall_positions
---
+++ tests/test_square_list.py
@@ -0,0 +1,73 @@
+from typing import List
+
+import pytest
+
+from project.model.board import Board, Position, Obstacle
+from project.model.direction import Direction
+from project.model.movement import Movement
+from project.model.position import Pattern, OutOfBoundBlockPositionException
+from project.model.square_type import SquareType
+
+
+class TestSquareList:
+    board_height: int = 5
+    board_width: int = 5
+    up_movement: Movement = Movement(direction=Direction.UP)
+    down_movement: Movement = Movement(direction=Direction.DOWN)
+    left_movement: Movement = Movement(direction=Direction.LEFT)
+    right_movement: Movement = Movement(direction=Direction.RIGHT)
+
+    position_start: Position = Position(1, 1)
+    position_empty: Position = Position(2, 2)
+    position_goal: Position = Position(5, 5)
+
+    coordinates: [[int, int]] = [[0, 0], [0, 4], [0, 10], [4, 0], [10, 0], [10, 10]]
+    positions_out: [Position] = []
+    for coordinate in coordinates:
+        positions_out.append(Position(co_x=coordinate[0], co_y=coordinate[1]))
+
+    position_next_to_start: Position = Position(1, 2)
+    obstacle_next_to_start: Obstacle = Obstacle(position=position_next_to_start, pattern=Pattern(
+        list_of_movements=[up_movement, down_movement]))
+
+    position_next_to_wall: Position = Position(1, 5)
+    obstacle_next_to_wall: Obstacle = Obstacle(position=position_next_to_wall, pattern=Pattern(
+        list_of_movements=[down_movement, down_movement]))
+
+    board: Board = Board(height=board_height, width=board_width,
+                         position_start=position_start, position_goal=position_goal,
+                         list_of_obstacle=[obstacle_next_to_start, obstacle_next_to_wall])
+
+    board.instantiate_singleton_viewer()
+
+    square_type_with_position = [
+        (SquareType.START, position_start), (SquareType.GOAL, position_goal), (SquareType.EMPTY, position_empty)]
+
+    @pytest.mark.parametrize("excepted_square_type, tested_position", square_type_with_position)
+    def test_should_get_square_type_with_position_corresponding_square_type(self, excepted_square_type,
+                                                                            tested_position):
+        assert excepted_square_type.value == self.board.square_list.get_square_type_from_board_by_position(
+            tested_position).value
+
+    @pytest.mark.parametrize("tested_position", positions_out)
+    def test_should_get_square_type_with_position_raise_exception(self, tested_position):
+        with pytest.raises(OutOfBoundBlockPositionException):
+            self.board.square_list.get_square_type_from_board_by_position(tested_position)
+
+    def test_should_get_index_of_list_of_square_by_position_with_position_start_return_0(self):
+        expected_index: int = 0
+        assert expected_index == self.board.square_list._get_index_of_list_of_square_by_position(
+            position=self.position_start)
+
+    @pytest.mark.parametrize("tested_position", positions_out)
+    def test_should_get_index_of_list_of_square_by_position_with_position_start_raise_exception(self, tested_position):
+        with pytest.raises(OutOfBoundBlockPositionException):
+            self.board.square_list._get_index_of_list_of_square_by_position(position=tested_position)
+
+    def test_should_move_obstacles_not_erase_start_position(self):
+        expected_log: List[SquareType] = [SquareType.START, SquareType.OBSTACLE, SquareType.START]
+        square_type_log: List[SquareType] = []
+        for i in range(3):
+            square_type_log.append(self.board.square_list.get_square_type_from_board_by_position(self.position_start))
+            self.board.move_obstacles()
+        assert expected_log == square_type_log
