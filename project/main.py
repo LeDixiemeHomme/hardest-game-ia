cc7e6b70a4493d2a4762a7f54b3efa9d8b663a17
LeDixiemeHomme <benoit.valleea@hotmail.fr>
2021-12-24 13:12:26 +0100

    feat(filling-qtable): implement on main the agent methods to make ia work

--- project/main.py
+++ project/main.py
@@ -4,6 +4,7 @@ from project.logger.logger import Logger
 from project.constants.game_constants import Movement, Board, MAIN_BOARD, SQUARE_BOARD, TEST_BOARD
 
 from project.model.agent import Agent
+from project.model.direction import Direction
 
 logger: Logger = Logger(name=__name__, log_file_name="main_log")
 stdout_logger = logger.stdout_log
@@ -37,10 +38,8 @@ if __name__ == '__main__':
         agent.board.move_obstacles()
         agent.draw_image_on_current_position()
 
-
-
-        # action = agent.best_action()
-        # reward = env.apply(agent, action)
+        best_direction: Direction = agent.best_action()
+        reward: int = agent.move_agent_with_qtable_update(requested_movement=Movement(direction=best_direction))
 
         if agent.is_position_on_goal_square():
             win = True
