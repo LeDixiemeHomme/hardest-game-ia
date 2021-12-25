e3d86c4455713c54f803d4f35359b326ea074601
LeDixiemeHomme <benoit.valleea@hotmail.fr>
2021-12-25 19:34:49 +0100

    feat(filling-qtable): add get_surrounding_positions and number_of_square_between_self_and_tested_position

--- project/model/position.py
+++ project/model/position.py
@@ -24,6 +24,30 @@ class Position:
 
         return applied
 
+    def get_surrounding_positions(self):
+        positions: List[Position] = []
+        #             *
+        #          *  *  *
+        #       *  *  ¤  *  *
+        #          *  *  *
+        #             *
+        for direction in Direction.__members__.values():
+            position_plus_one: Position = self.apply_movement(Movement(direction=direction))
+            positions.append(position_plus_one)
+            if direction != direction.STAY:
+                position_plus_two: Position = position_plus_one.apply_movement(Movement(direction=direction))
+                positions.append(position_plus_two)
+        positions.append(Position(co_x=self._co_x - 1, co_y=self._co_y))
+        positions.append(Position(co_x=self._co_x + 1, co_y=self._co_y))
+        positions.append(Position(co_x=self._co_x, co_y=self._co_y - 1))
+        positions.append(Position(co_x=self._co_x, co_y=self._co_y + 1))
+        return positions
+
+    def number_of_square_between_self_and_tested_position(self, tested_position_co_x: int,
+                                                          tested_position_co_y: int) -> int:
+        tested_position: Position = Position(co_x=tested_position_co_x, co_y=tested_position_co_y)
+        return abs(tested_position.co_x - self._co_x) + abs(tested_position.co_y - self._co_y)
+
     def check_boundaries(self, width: int, height: int):
         if not self._is_position_inside_board_boundaries(width=width, height=height):
             raise OutOfBoundBlockPositionException(position=self,
@@ -39,6 +63,9 @@ class Position:
     @property
     def co_y(self) -> int:
         return self._co_y
+
+    def to_tuple(self) -> (int, int):
+        return self._co_x, self._co_y
 
     def __str__(self) -> str:
         string: str = "Position : { " + " co_x = " + str(self._co_x) + "; co_y = " + str(self._co_y) + " }"
--- tests/test_position.py
+++ tests/test_position.py
@@ -13,33 +13,53 @@ class TestPosition:
     left_movement: Movement = Movement(direction=Direction.LEFT)
     right_movement: Movement = Movement(direction=Direction.RIGHT)
 
-    position_empty: Position = Position(2, 2)
+    position_in: Position = Position(2, 2)
+    position_out: Position = Position(2, 10)
 
     coordinates: [[int, int]] = [[0, 0], [0, 4], [0, 10], [4, 0], [10, 0], [10, 10]]
     positions_out: [Position] = []
     for coordinate in coordinates:
         positions_out.append(Position(co_x=coordinate[0], co_y=coordinate[1]))
 
-    position_after_movement = [(up_movement, Position(co_x=position_empty.co_x, co_y=position_empty.co_y - 1)),
-                               (down_movement, Position(co_x=position_empty.co_x, co_y=position_empty.co_y + 1)),
-                               (left_movement, Position(co_x=position_empty.co_x - 1, co_y=position_empty.co_y)),
-                               (right_movement, Position(co_x=position_empty.co_x + 1, co_y=position_empty.co_y))]
+    position_with_is_inside: [Position, bool] = [[position_in, True], [position_out, False]]
+
+    position_after_movement = [(up_movement, Position(co_x=position_in.co_x, co_y=position_in.co_y - 1)),
+                               (down_movement, Position(co_x=position_in.co_x, co_y=position_in.co_y + 1)),
+                               (left_movement, Position(co_x=position_in.co_x - 1, co_y=position_in.co_y)),
+                               (right_movement, Position(co_x=position_in.co_x + 1, co_y=position_in.co_y))]
+
+    position_3_3: Position = Position(co_x=3, co_y=3)
+    position_3_4: Position = Position(co_x=3, co_y=4)
+    position_4_4: Position = Position(co_x=4, co_y=4)
+    position_10_10: Position = Position(co_x=10, co_y=10)
+
+    two_positions_with_number_of_squares_between: [Position, int] = [[position_3_3, 0],
+                                                                     [position_3_4, 1],
+                                                                     [position_4_4, 2],
+                                                                     [position_10_10, 14]]
 
     @pytest.mark.parametrize("tested_position", positions_out)
     def test_should_get_position_after_movement_with_position_out_raise_exception(self, tested_position):
         with pytest.raises(OutOfBoundBlockPositionException):
             tested_position.check_boundaries(width=self.width, height=self.height)
 
-    @pytest.mark.parametrize("movement, position_expected", position_after_movement)
-    def test_should_get_position_after_movement(self, movement, position_expected):
-        result_position: Position = self.position_empty.apply_movement(movement=movement)
-        assert position_expected == result_position
-
-    def test_should_is_position_inside_board_boundaries_with_position_start_return_true(self):
-        assert self.position_empty._is_position_inside_board_boundaries(
-            width=self.width, height=self.height)
-
     @pytest.mark.parametrize("tested_position", positions_out)
     def test_should_get_square_type_with_position_corresponding_square_type(self, tested_position):
         assert not tested_position._is_position_inside_board_boundaries(
             width=self.width, height=self.height)
+
+    @pytest.mark.parametrize("movement, position_expected", position_after_movement)
+    def test_should_get_position_after_movement(self, movement, position_expected):
+        result_position: Position = self.position_in.apply_movement(movement=movement)
+        assert position_expected == result_position
+
+    @pytest.mark.parametrize("position, is_inside", position_with_is_inside)
+    def test_should_is_position_inside_board_boundaries_with_position_works(self, position, is_inside):
+        assert position._is_position_inside_board_boundaries(width=self.width, height=self.height) is is_inside
+
+    @pytest.mark.parametrize("position, number", two_positions_with_number_of_squares_between)
+    def test_should_number_of_square_between_self_and_tested_position_works(self, position, number):
+        number_to_test: int = self.position_3_3.number_of_square_between_self_and_tested_position(
+            tested_position_co_x=position.co_x,
+            tested_position_co_y=position.co_y)
+        assert number_to_test == number
