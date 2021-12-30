6f841bf5339626cfb6c8d66ba27fc145ae22ea67
LeDixiemeHomme <benoit.valleea@hotmail.fr>
2021-12-30 09:05:12 +0100

    feat(filling-qtable): resolve hash of StateWithSurrounding

--- project/model/square.py
+++ project/model/square.py
@@ -2,14 +2,36 @@ from copy import copy
 from typing import List
 
 from project.model.position import Position
-# from project.model.square_list import SquareList
 from project.model.square_type import SquareType
 
 
-# class StateWithSurrounding:
-#     def __init__(self, center_square: 'Square', square_list: List['Square']):
-#         self._square = center_square
-#         self._list_square = square_list
+class StateWithSurrounding:
+    def __init__(self, center_square: 'Square', list_square: List['Square']):
+        self._center_square = center_square
+        self._list_square = list_square
+
+    @property
+    def center_square(self):
+        return self._center_square
+
+    @property
+    def list_square(self):
+        return self._list_square
+
+    def __str__(self) -> str:
+        string: str = "Square : { " + str(self._center_square) + " }"
+        return string
+
+    def __eq__(self, tested):
+        if isinstance(tested, StateWithSurrounding):
+            return self._center_square == tested.center_square and self._list_square == tested.list_square
+        return False
+
+    def __hash__(self):
+        concat: int = hash(self._center_square)
+        for square in self._list_square:
+            concat += hash(square)
+        return hash(concat)
 
 
 class Square:
@@ -25,8 +47,8 @@ class Square:
             result_dict[i % length_of_surrounding] = square_type
         return result_dict
 
-    def _part_state(self, iterator: int, i: int, nb_surrounding_position: int) -> [{'Square': ['Square']}]:
-        list_of_possible_state: List[{Square: [Square]}] = []
+    def _part_state(self, iterator: int, i: int, nb_surrounding_position: int) -> [StateWithSurrounding]:
+        list_of_possible_state: List[StateWithSurrounding] = []
         for y in range(iterator):
             dict_ind_squ_type = \
                 self.create_dict_index_with_square_type(starting_point=y, number_of_square=i,
@@ -56,9 +78,9 @@ class Square:
                     self._create_state(index_in_surrounding_with_square_type=dict_temp))
         return list_of_possible_state
 
-    def all_possible_state(self) -> [{'Square': ['Square']}]:
+    def all_possible_state(self) -> [StateWithSurrounding]:
         # ça retourne une liste qui va etre iteré pour avoir les key de la qtable
-        list_of_possible_state: List[{Square: [Square]}] = []
+        list_of_possible_state: List[StateWithSurrounding] = []
         nb_surrounding_position: int = len(self.position.get_surrounding_positions())
         for i in range(nb_surrounding_position+1):
             # elif i == 3:
@@ -105,12 +127,7 @@ class Square:
                     list_of_possible_state.append(state)
         return list_of_possible_state
 
-    # def temp_create_state(self, dict_index_square_type: {int: SquareType}) -> StateWithSurrounding:
-    #     state: StateWithSurrounding = StateWithSurrounding(center_square=self, index_square_type=dict_index_square_type)
-    #
-    #     return state
-
-    def _create_state(self, index_in_surrounding_with_square_type: {int: SquareType}) -> {'Square': ['Square']}:
+    def _create_state(self, index_in_surrounding_with_square_type: {int: SquareType}) -> StateWithSurrounding:
         copy_self: Square = copy(self)
         if len(index_in_surrounding_with_square_type) > len(copy_self.position.get_surrounding_positions()):
             raise Exception
@@ -118,7 +135,7 @@ class Square:
             if index_square_type > len(copy_self.position.get_surrounding_positions()):
                 raise Exception
 
-        state: {Square: [Square]} = {copy_self: []}
+        state: StateWithSurrounding = StateWithSurrounding(center_square=copy_self, list_square=[])
         for i in range(len(copy_self.position.get_surrounding_positions())):
             contains: bool = False
             position: Position = copy_self.position.get_surrounding_positions()[i]
@@ -129,9 +146,9 @@ class Square:
             if contains:
                 square_type = index_in_surrounding_with_square_type.get(i)
             square_to_add: Square = Square(position=position, square_type=square_type)
-            state[copy_self].append(square_to_add)
+            state.list_square.append(square_to_add)
         if copy_self.square_type == SquareType.START:
-            state[copy_self][0] = copy_self
+            state.list_square[0] = copy_self
         return state
 
     @property
